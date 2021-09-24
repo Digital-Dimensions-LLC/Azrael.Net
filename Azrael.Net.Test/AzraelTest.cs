28866a7384031faa53aefa9896dc411feb733998
Emily <89871431+emillly-b@users.noreply.github.com>
2021-09-24 13:28:35 -0500

    Add Xunit and Testing Solutions

--- Azrael.Net.Test/AzraelTest.cs
+++ Azrael.Net.Test/AzraelTest.cs
@@ -1,13 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Azrael.Net.Api;
+using Azrael.Net.Data;
+using Xunit;
+
+using Assert = Xunit.Assert;
+
 
 namespace Azrael.Net.Test
 {
+
     [TestClass]
-    public class UnitTest1
+    public class AzraelTest
     {
+        private string apiKey = "";
+
         [TestMethod]
-        public void TestMethod1()
+        [Theory]
+        [InlineData("548009285892833280", true)]  // Dev ban
+        [InlineData("641795527444529152", false)] // Api Owner
+        [InlineData("341275030941859850", false)] // Self
+
+        public async void TestCheckBan(string id, bool banned)
         {
+            bool _testValue = await AzraelAPI.CheckBan(id, apiKey);
+            Assert.Equal(_testValue, banned);
         }
     }
 }
