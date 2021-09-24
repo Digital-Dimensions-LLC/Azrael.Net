764d2800181f3827b85dccbc314639d795765bf4
Emily <89871431+emillly-b@users.noreply.github.com>
2021-09-24 15:16:30 -0500

    Split Data Classes

--- Azrael.Net/Data/Ban.cs
+++ Azrael.Net/Data/Ban.cs
@@ -3,7 +3,6 @@ using System.Text.Json.Serialization;
 
 namespace Azrael.Net.Data
 {
-    // Root myDeserializedClass = JsonConvert.DeserializeObject<Ban>(myJsonResponse); 
     public class BanRecord
     {
         [JsonPropertyName("status")]
@@ -15,31 +14,5 @@ namespace Azrael.Net.Data
         [JsonPropertyName("bandata")]
         public BanData BanData { get; set; }
     }
-    public class BanData
-    {
-        [JsonPropertyName("id")]
-        public string Id { get; set; }
-
-        [JsonPropertyName("reason")]
-        public string Reason { get; set; }
-
-        [JsonPropertyName("proof")]
-        public string Proof { get; set; }
-
-        [JsonPropertyName("timestamp")]
-        public string Timestamp { get; set; }
-
-        [JsonPropertyName("moderator")]
-        public string Moderator { get; set; }
-
-        [JsonPropertyName("notes")]
-        public string Notes { get; set; }
-
-        public BanData(string UserID, string BanReason, string ProofURL)
-        {
-            Id = UserID;
-            Reason = BanReason;
-            Proof = ProofURL;
-        }
-    }
+    
 }
---
+++ Azrael.Net/Data/BanData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Azrael.Net.Data
+{    public class BanData
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("reason")]
+        public string Reason { get; set; }
+
+        [JsonPropertyName("proof")]
+        public string Proof { get; set; }
+
+        [JsonPropertyName("timestamp")]
+        public string Timestamp { get; set; }
+
+        [JsonPropertyName("moderator")]
+        public string Moderator { get; set; }
+
+        [JsonPropertyName("notes")]
+        public string Notes { get; set; }
+
+        public BanData(string UserID, string BanReason, string ProofURL)
+        {
+            Id = UserID;
+            Reason = BanReason;
+            Proof = ProofURL;
+        }
+    }
+}
